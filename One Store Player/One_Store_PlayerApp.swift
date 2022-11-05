3118f92c83faa07fbf22936e4bd8f170517194ad
Muhammad Ali Rana <alisaleem877@gmail.com>
2022-11-05 15:22:54 +0500

    Initial Commit

---
+++ One Store Player/One_Store_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_Store_PlayerApp.swift
+//  One Store Player
+//
+//  Created by MacBook Pro on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct One_Store_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
